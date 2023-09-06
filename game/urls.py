4dc059b6b88289c6b2bf6ff806855527128e83f2
tangdou <tangdouwhy919@gmail.com>
2023-09-06 23:42:05 +0800

    第一次加入图片

--- game/urls.py
+++ game/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from game.views import index
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("",index,name="game_index"),
 ]
--- game/views.py
+++ game/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-return HttpResponse("我的第一个网页!!!")
+    line1='''<h1 style="text-align: center">我的第一个网页!!!!</h1>
+    <img src="https://cdn.acwing.com/media/file_system/file/application/icon/01be01554421020000019ae93ff35c.jpg1280w_1l_2o_100sh_LBjQxh6.jpg" width=800>
+    '''
+    return HttpResponse(line1)
+
