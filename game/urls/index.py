db1b11c80b64fcddd17d95832c4e4821feec4781
tangdou <tangdouwhy919@gmail.com>
2023-09-15 17:46:16 +0800

    完成文件结构

--- game/urls/index.py
+++ game/urls/index.py
@@ -1,6 +1,7 @@
 from django.urls import path,include
-
+from game.views.index import index
 urlpatterns=[
+    path("",index,name="index"),
     path("menu/",include("game.urls.menu.index")),
     path("playground/",include("game.urls.playground.index")),
     path("settings/",include("game.urls.settings.index")),
--- game/urls/menu/index.py
+++ game/urls/menu/index.py
@@ -1,4 +1,6 @@
 from django.urls import path,include
+from game.views.menu.index import index
 
 urlpatterns=[
-        ]
+    path("",index,name="menuindex"),
+]
