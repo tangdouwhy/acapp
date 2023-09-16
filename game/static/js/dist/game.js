2ad2fbddba19e37c3945e265c7768d6a6ab39b6f
tangdou <tangdouwhy919@gmail.com>
2023-09-16 11:25:54 +0800

    完成菜单页面

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -4,19 +4,42 @@ class AcGameMenu{
         this.$menu=$(`
 <div class="ac-game-menu">
     <div class="ac-game-menu-field">
-        <div class="ac-game-menu-field-item">
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-single-mode">
             单人模式
         </div>
-        <div class="ac-game-menu-field-item">
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-multi-mode">
             多人模式
         </div>
-        <div class="ac-game-menu-field-item">
+        <br>
+        <div class="ac-game-menu-field-item ac-game-menu-field-item-settings-mode">
             设置
         </div>
     </div>
 </div>
 `);
         this.root.$ac_game.append(this.$menu);
+        this.$single_mode=this.$menu.find('.ac-game-menu-field-item-single-mode');
+        this.$multi_mode=this.$menu.find('.ac-game-menu-field-item-multi-mode');
+        this.$settings_mode=this.$menu.find('.ac-game-menu-field-item-settings-mode');
+            
+        this.start();
+    }
+
+    start(){
+        this.add_listening_events();
+    }
+    add_listening_events(){
+        let outer=this;
+        this.$single_mode.click(function(){
+            console.log("click single mode");
+        });
+        this.$multi_mode.click(function(){
+            console.log("click multi mode");
+        });
+        this.$settings_mode.click(function(){
+            console.log("click settings mode");
+        });
     }
 }
 
