12541ae1dbb23b849e5cdf966469defccb4795a0
tangdou <tangdouwhy919@gmail.com>
2023-09-16 22:23:58 +0800

    game menu first

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -55,14 +55,122 @@ class AcGameMenu{
     }
 }
 
+let AC_GAME_OBJECTS=[];
+
+class AcGameObject{
+    constructor(){
+        AC_GAME_OBJECTS.push(this);
+
+        this.has_called_start = false; //是否执行过start函数
+
+        this.timedalta = 0;// 当前帧距离上一帧的时间间隔(ms)
+    }
+
+    start(){// 只会在第一帧执行一次
+    }
+
+    update(){// 每一帧均会执行一次
+    }
+
+    on_destroy(){// 在被销毁前执行一次
+    }
+
+    destroy(){// 删掉该物体
+        this.on_destory();
+        for(let i=0;i<AC_GAME_OBJECTS.length;i++){
+            if(AC_GAME_OBJECTS[i] === this){
+                AC_GAME_OBJECTS.splice(i,1);
+                break;
+            }
+        }
+    }
+}
+
+let last_timestamp;
+
+let AC_GAME_ANIMATION = function(timestamp){
+    for(let i=0;i<AC_GAME_OBJECTS.length;i++){
+        let obj = AC_GAME_OBJECTS[i];
+        if(!obj.has_called_start){
+            obj.start();
+            obj.has_called_start=true;
+        }else{
+            obj.timedelta = timestamp - last_timestamp;
+            obj.update();
+        }
+    }
+    last_timestamp = timestamp;
+
+    requestAnimationFrame(AC_GAME_ANIMATION);
+}
+
+requestAnimationFrame(AC_GAME_ANIMATION);
+class GameMap extends AcGameObject{
+    constructor(playground){
+        super();
+        this.playground = playground;
+        this.$canvas = $(`<canvas></canvas>`);
+        this.ctx = this.$canvas[0].getContext('2d');
+        this.ctx.canvas.width = this.playground.width;
+        this.ctx.canvas.height = this.playground.height;
+        this.playground.$playground.append(this.$canvas);
+
+    }
+
+    start(){
+    }
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.fillStyle="rgba(0,0,0,6)";
+        this.ctx.fillRect(0,0,this.ctx.canvas.width,this.ctx.canvas.height);
+    }
+}
+class Player extends AcGameObject{
+    constructor(playground,x,y,radius,color,speed,is_me){
+        super();
+        this.playground=playground;
+        this.ctx=this.playground.game_map.ctx;
+        this.x=x;
+        this.y=y;
+        this.radius=radius;
+        this.color=color;
+        this.speed=speed;
+        this.is_me=is_me;
+        this.eps=0.1;
+    }
+
+    start(){
+    }
+
+    update(){
+        this.render();
+    }
+
+    render(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x,this.y,this.radius,0,Math.PI * 2,false);
+        this.ctx.fillStyle = this.color;
+        this.ctx.fill();
+    }
+}
 class AcGamePlayground{
     constructor(root){
         this.root=root;
         this.$playground=$(`
-<div>游戏界面</div>
+<div class="ac-game-playground"></div>
 `);
-        this.hide();
+        // this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.width=this.$playground.width();
+        this.height=this.$playground.height();
+        this.game_map=new GameMap(this);
+        this.players=[];
+
+        this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,"white",this.height*0.15,true));
 
         this.start();
     }
@@ -83,11 +191,11 @@ class AcGamePlayground{
         this.$playground.hide();
     }
 }
-class AcGame{
+export class AcGame{
     constructor(id){
         this.id=id;
         this.$ac_game=$('#'+id);
-        this.menu=new AcGameMenu(this);
+        // this.menu=new AcGameMenu(this);
         this.playground=new AcGamePlayground(this);
 
         this.start();
--- game/static/js/src/playground/zbase.js
+++ game/static/js/src/playground/zbase.js
@@ -2,10 +2,16 @@ class AcGamePlayground{
     constructor(root){
         this.root=root;
         this.$playground=$(`
-<div>游戏界面</div>
+<div class="ac-game-playground"></div>
 `);
-        this.hide();
+        // this.hide();
         this.root.$ac_game.append(this.$playground);
+        this.width=this.$playground.width();
+        this.height=this.$playground.height();
+        this.game_map=new GameMap(this);
+        this.players=[];
+
+        this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,"white",this.height*0.15,true));
 
         this.start();
     }
