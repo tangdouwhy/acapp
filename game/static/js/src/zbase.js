db137f54e604fe75737ff36d9775214bee7dda8b
tangdouwhy <tangdouwhy919@gmail.com>
2023-09-27 20:17:30 +0800

    添加python合并程序

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,12 +1,14 @@
-export class AcGame{
-    constructor(id){
-        this.id=id;
-        this.$ac_game=$('#'+id);
+export class AcGame {
+    constructor(id) {
+        this.id = id;
+        this.$ac_game = $('#' + id);
         // this.menu=new AcGameMenu(this);
-        this.playground=new AcGamePlayground(this);
+        this.playground = new AcGamePlayground(this);
 
         this.start();
     }
 
-    start(){}
+
+    start() {
+    }
 }
