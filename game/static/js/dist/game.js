f394cbb6e2e5c33508976a913378ae02d2f504b5
tangdouwhy <tangdouwhy919@gmail.com>
2023-09-27 21:32:39 +0800

    修改合并脚本

--- game/static/js/dist/game.js
+++ game/static/js/dist/game.js
@@ -157,6 +157,40 @@ class Player extends AcGameObject{
         this.ctx.fill();
     }
 }
+class AcGamePlayground{
+    constructor(root){
+        this.root=root;
+        this.$playground=$(`
+<div class="ac-game-playground"></div>
+`);
+        // this.hide();
+        this.root.$ac_game.append(this.$playground);
+        this.width=this.$playground.width();
+        this.height=this.$playground.height();
+        this.game_map=new GameMap(this);
+        this.players=[];
+
+        this.players.push(new Player(this,this.width/2,this.height/2,this.height*0.05,"white",this.height*0.15,true));
+
+        this.start();
+    }
+
+    start(){
+
+    };
+
+    update(){};
+
+    show(){
+    //打开playground界面
+        this.$playground.show();
+    }
+
+    hide(){
+    //关闭playground界面
+        this.$playground.hide();
+    }
+}
 export class AcGame {
     constructor(id) {
         this.id = id;
